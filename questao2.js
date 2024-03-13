efa536f25ac3392a23cbad4d582a50a4ed2be09f
Kanzeer <miguelroaamaral@gmail.com>
2024-03-13 16:48:24 -0400

    questao 3 resolvida

--- questao2.js
+++ questao2.js
@@ -1,5 +1,4 @@
 //Questão 2: Como você pode verificar o tipo de uma variável em JavaScript?
 
-
 var minhaVariavel = tofu;
 console.log(typeof minhaVariavel);
---
+++ questao3.js
@@ -0,0 +1,8 @@
+//Questão 3: Como você pode concatenar duas strings em JavaScript?
+
+let nome = "miguel"
+let sobrenome = "roa"
+let resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
