05915257fef20cc086e3c8888a492fc37087b16e
Kanzeer <miguelroaamaral@gmail.com>
2024-03-13 16:23:29 -0400

    questao 1 resolvida

--- let.js
+++ let.js
@@ -1,16 +1,15 @@
 // Declaração de uma variável
-let nome; // Escopo de bloco
+//let nome; // Escopo de bloco
 var idade; // Escopo global
-const PI = 3.14; // Escopo de bloco e global
-
+const PI = 3.14; // Escopo de global
 
 function soma() {
   if(true) {
-    //const PI = 3.14; // Escopo global
-    console.log(PI)
+    var nome = "kampf"
+    console.log(nome)
   }
-  PI = 50
-  console.log(PI)
-}
 
+  nome = "death"
+  console.log(nome)
+}
 soma()
