148b54cf4f6d013e68e4a78ce7b18e2c0cf06ced
Kanzeer <miguelroaamaral@gmail.com>
2024-03-18 15:51:00 -0400

    questao 10 resolvida

---
+++ questao10.js
@@ -0,0 +1,6 @@
+//Questão 10: Como você pode remover o último elemento de um array em JavaScript?
+
+let frutas = ["Maçã", "Banana", "Kiwi", "Laranja"];
+
+frutas.pop(); //"pop" é utilizado para apagar o ultimo termo descrito no armazenamento frutas. Claro, esse ultimo termo sendo "Laranja"
+console.log(frutas);
