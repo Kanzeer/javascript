fdf1f3cc39125de3edcd3324f9b159678d92bc9b
Kanzeer <miguelroaamaral@gmail.com>
2024-03-18 14:44:45 -0400

    Adicionado arquivo e exemplo de Objeto

---
+++ objeto.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "Miguel",
+    idade: 16,
+    profissao: "Vagabundo"
+};
+
+console.log(pessoa.nome); //saida: nome
+console.log(pessoa.idade); //saida: idade
+console.log(pessoa.profissao); //saida: profissao
