fcc4837f29ed723e008a205093c935f92a0e2493
Kanzeer <miguelroaamaral@gmail.com>
2024-03-18 16:00:22 -0400

    questao 11 resolvida

---
+++ questao11.js
@@ -0,0 +1,5 @@
+//Questão 11: Qual é a diferença entre let, const e var em JavaScript? 
+
+let nome; // Escopo de bloco, podem ser acessadas apenas dentro do bloco do codigo que estiver
+var idade; // Escopo global, podem ser acessadas de qualquer lugar do codigo
+const PI = 3.14; // Escopo de global, é uma constancia, da ou muda o significado
