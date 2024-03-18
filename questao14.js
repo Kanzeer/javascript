f64652850c063ac0b9d88f723e408d77a0ef9202
Kanzeer <miguelroaamaral@gmail.com>
2024-03-18 17:06:27 -0400

    questao 14 resolvida

---
+++ questao14.js
@@ -0,0 +1,8 @@
+//Questão 14: Como você pode converter uma string em minúsculas em JavaScript?
+
+const string = "hElLo WoRLd"
+
+const minuscula = string.toLowerCase()
+
+console.log(string) //comentario escrito de forma incorreta
+console.log(minuscula) //comando que corrigira o comentario e o fara vir em minusculo
