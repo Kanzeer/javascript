1a9650c26ac8823037cfbf6e7f75846fd3c4da54
Kanzeer <miguelroaamaral@gmail.com>
2024-03-18 15:40:40 -0400

    questao 9 resolvida

---
+++ questao9.js
@@ -0,0 +1,7 @@
+//Questão 9: Como você pode adicionar um novo elemento ao final de um array em JavaScript?
+
+var frutas = ["Maçã", "Banana", "Laranja"];
+
+frutas.push("Kiwi"); //adiciona um novo item ao codigo final
+
+console.log(frutas); 
