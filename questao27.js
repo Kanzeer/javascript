6a80dacfb1fffe105cd782254d573b30f1163a23
Kanzeer <miguelroaamaral@gmail.com>
2024-03-20 17:28:52 -0400

    questao 27 resolvida

---
+++ questao27.js
@@ -0,0 +1,5 @@
+//Questão 27: Como você pode remover um elemento específico de um array em JavaScript? 
+
+let array = [0, 1, 2, 3, 4 ,5 ,6 ,7 ,8];
+array.splice(4,1); //"4" sendo o determinado termo que queremos apagar. "1" sendo a quantidade apartir do termo
+console.log( array );
