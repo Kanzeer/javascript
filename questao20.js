540da4114a7da8837601da551af3b0d01c332f0e
Kanzeer <miguelroaamaral@gmail.com>
2024-03-20 16:31:30 -0400

    questao 20 resolvida e alterada

--- questao20.js
+++ questao20.js
@@ -1,14 +1,7 @@
 //Questão 20: Como você pode chamar uma função de retorno de chamada em JavaScript? 
 
-function termo(callback) { //"callback" é um parametro atribuida a uma função
-    console.log("insano")
-
-    callback(); //função que chama o primeiro callback e conecta ao escopo global
-  }
-  
   function minhaCallback() { //função de chamada de retorno
     console.log("maneiro")
   }
-  
- termo(minhaCallback)
+
     
