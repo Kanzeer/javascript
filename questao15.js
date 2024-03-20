7d5008e5dd8b62549eefe3668e4825ce7d57af03
Kanzeer <miguelroaamaral@gmail.com>
2024-03-20 14:25:44 -0400

    questao 15 resolvida

---
+++ questao15.js
@@ -0,0 +1,7 @@
+//Questão 15: Como você pode obter o comprimento de uma string em JavaScript?
+
+let termo = "nikael tem medo de altura"; //sentença que queremos medir
+
+let comp = termo.length; //codigo que medira nossa sentença (ela mede os espaços também)
+
+console.log(comp);
