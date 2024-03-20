61fe97d7a6b6734f54c11571901afbd2bc701a0e
Kanzeer <miguelroaamaral@gmail.com>
2024-03-20 16:31:50 -0400

    questao 22 resolvida

---
+++ questao22.js
@@ -0,0 +1,9 @@
+//Questão 22: Como você pode verificar se uma variável é nula em JavaScript?
+
+let variável = null;
+
+if (variável === null) {
+    //código executado seja nulo
+}
+
+console.log(variável);
