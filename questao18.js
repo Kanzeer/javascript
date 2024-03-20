1222782b5c388960afd7149572f946f33e7647ca
Kanzeer <miguelroaamaral@gmail.com>
2024-03-20 14:49:40 -0400

    questao 18 resolvida

---
+++ questao18.js
@@ -0,0 +1,9 @@
+//Questão 18: Como você pode acessar o valor de uma propriedade de um objeto em JavaScript?
+
+const obj = {
+    nome: "Laika", //"nome" sendo nossa propriedade e "Laika" o valor atribuido na propriedade
+    idade: 2
+};
+
+console.log(obj); //obter toda "const obj"
+console.log(obj.idade); //obter uma propriedade especifica
